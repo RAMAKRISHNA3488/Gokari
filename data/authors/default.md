d6edb2bcfbe6687780174bad1c481e8bbe87e97f
ramakrishna3488 <132547350+RAMAKRISHNA3488@users.noreply.github.com>
2024-12-03 16:42:19 +0530

    RK

---
+++ data/authors/default.md
@@ -0,0 +1,13 @@
+---
+name: Gokari
+avatar: /static/images/avatar.jpg
+occupation: Data Engineer
+company: Accenture
+email: gokarisaheb283@gmail.com
+twitter: https://twitter.com/Gokarisahe35544
+linkedin: 
+github: 
+text1: I am an India based Data Engineer with a bachelors in Computer Science. I am passionate about Data Science and Automation. I am also fascinated with Mathematics and wish to make a career out of it someday.
+text2:
+text3:
+---
---
+++ data/projectsData.js
@@ -0,0 +1,25 @@
+const projectsData = [
+  {
+    title: 'Library Management System',
+    description: `Developed with PHP, SQL, HTML, CSS and JS. Features include search functionality, 
+enhancing user experience and engagement.`,
+    imgSrc: '',
+    href: '',
+    github: 'No',
+    tech1: 'PHP',
+    tech2: 'SQL',
+    tech3: 'HTML, CSS, JS',
+  },
+  {
+    title: 'Netflix Clone',
+    description: `Develop a Netflix Clone Application using PHP, SQL, HTML and CSS delivering a smooth and seamless video streaming experience to users.`,
+    imgSrc: '',
+    href: '',
+    github: 'https://github.com/pycoder2000/Car-Price-Prediction',
+    tech1: 'PHP',
+    tech2: 'SQL',
+    tech3: 'HTML and CSS',
+  },
+]
+
+export default projectsData
